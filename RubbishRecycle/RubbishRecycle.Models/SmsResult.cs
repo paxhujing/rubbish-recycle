2d87805230984a6bbba01060a034a563b3c3cd83
paxhujing <935714690@qq.com>
2016-04-07 22:28:11 +0800

    join alydayu sms api

---
+++ RubbishRecycle/RubbishRecycle.Models/SmsResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubbishRecycle.Models
+{
+    /// <summary>
+    /// 短信请求结果。
+    /// </summary>
+    public class SmsResult
+    {
+        [JsonProperty("code")]
+        public String Code { get; set; }
+
+        [JsonProperty("extend")]
+        public String Extend { get; set; }
+
+        [JsonProperty("is_success")]
+        public Boolean IsSuccess { get; set; }
+
+        [JsonProperty("error")]
+        public String Error { get; set; }
+    }
+}
