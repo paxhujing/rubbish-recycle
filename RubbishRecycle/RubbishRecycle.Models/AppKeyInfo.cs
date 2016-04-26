07f2f8b561de96c7d1e0d46aec7409c0836b14b3
paxhujing <935714690@qq.com>
2016-04-26 22:34:07 +0800

    no message

---
+++ RubbishRecycle/RubbishRecycle.Models/AppKeyInfo.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubbishRecycle.Models
+{
+    [Table("app_key")]
+    public class AppKeyInfo
+    {
+        [Key]
+        [Column("app_key")]
+        public String AppKey { get; set; }
+
+        [Column("create_time")]
+        public DateTime CreateTime { get; set; }
+    }
+}
